5c566098848cf683f5e79c42936b40a6b6f9701b
Ekko <{YiFanChen99}+{YiFanChen99}@users.noreply.github.com>
2025-10-18 01:13:43 +0800

    fix: fix url match rules

--- Script.user.js
+++ Script.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Steamgifts-sheet-fetcher
 // @namespace    https://github.com/YiFanChen99/tampermonkey--steamgifts-sheet-fetcher
-// @version      1.1.3
+// @version      1.1.4
 // @description  Fetch games from Google Sheet via App Script
 // @author       YiFanChen99
-// @match        *://www.steamgifts.com/(giveaways/search*|giveaway/*)
+// @match        *://www.steamgifts.com/giveaways/search*
+// @match        *://www.steamgifts.com/giveaway/*
 // @grant        GM_xmlhttpRequest
 // @icon         https://raw.githubusercontent.com/YiFanChen99/tampermonkey--steamgifts-sheet-fetcher/main/favicon.ico
 // @downloadURL  https://raw.githubusercontent.com/YiFanChen99/tampermonkey--steamgifts-sheet-fetcher/main/Script.user.js
